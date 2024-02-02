5276c7105c38cc3d6bcbbc1f6672c95f5138b530
reijiokito <40384598+reijiokito@users.noreply.github.com>
2024-02-02 09:17:05 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@ git remote add target https://${INPUT_TARGET_USERNAME}:${INPUT_TARGET_TOKEN}@${I
 
 case "${GITHUB_EVENT_NAME}" in
     push)
+        git pull target main
         git push target main
         git push --tags target
         ;;
